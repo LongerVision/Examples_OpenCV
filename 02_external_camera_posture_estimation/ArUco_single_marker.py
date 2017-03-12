9617c61362bcc089eff99d61b4472d654c698e61
jiapei100 <jp4work@gmail.com>
2017-03-12 01:18:01 -0800

    minor typos modified

--- 02_external_camera_posture_estimation/ArUco_single_marker.py
+++ 02_external_camera_posture_estimation/ArUco_single_marker.py
@@ -23,7 +23,7 @@ map1, map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist_coeffs, r, 
 
 aruco_dict = aruco.Dictionary_get( aruco.DICT_6X6_1000 )
 arucoParams = aruco.DetectorParameters_create()
-markerLength = 20 #Here, our measurement unit is centimetre.
+markerLength = 20   # Here, our measurement unit is centimetre.
 
 
 
@@ -34,15 +34,17 @@ nbOfImgs = len(imgFileNames)
 for i in range(0, nbOfImgs):
     img = cv2.imread(imgFileNames[i], cv2.IMREAD_COLOR)
     imgRemapped = cv2.remap(img, map1, map2, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT) # for fisheye remapping
-    imgRemapped_gray = cv2.cvtColor(imgRemapped, cv2.COLOR_BGR2GRAY)  # aruco.etectMarkers() requires gray image
+    imgRemapped_gray = cv2.cvtColor(imgRemapped, cv2.COLOR_BGR2GRAY)    # aruco.etectMarkers() requires gray image
     corners, ids, rejectedImgPoints = aruco.detectMarkers(imgRemapped_gray, aruco_dict, parameters=arucoParams) # Detect aruco
     if ids != None: # if aruco marker detected
         rvec, tvec = aruco.estimatePoseSingleMarkers(corners, markerLength, camera_matrix, dist_coeffs) # For a single marker
         imgWithAruco = aruco.drawDetectedMarkers(imgRemapped, corners, ids, (0,255,0))
-        imgWithAruco = aruco.drawAxis(imgWithAruco, camera_matrix, dist_coeffs, rvec, tvec, 100)  # axis length 100 can be changed according to your requirement
-    else: # if aruco marker is NOT detected
-        imgWithAruco = imgRemapped # assign imRemapped_color to imgWithAruco directly
-        cv2.imshow("aruco", imgWithAruco) # display
-    if cv2.waitKey(2) & 0xFF == ord('q'): # if 'q' is pressed, quit.
+        imgWithAruco = aruco.drawAxis(imgWithAruco, camera_matrix, dist_coeffs, rvec, tvec, 100)    # axis length 100 can be changed according to your requirement
+    else:   # if aruco marker is NOT detected
+        imgWithAruco = imgRemapped  # assign imRemapped_color to imgWithAruco directly
+
+    cv2.imshow("aruco", imgWithAruco)   # display
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):   # if 'q' is pressed, quit.
         break
 
