d1682514699f1f291415f0324798c71b471ed799
jiapei100 <jp4work@gmail.com>
2017-03-20 03:15:31 -0700

    Add the license

--- 01_internal_camera_calibration/chessboard.py
+++ 01_internal_camera_calibration/chessboard.py
@@ -1,3 +1,38 @@
+################################################################################
+#                                                                              #
+#                                                                              #
+#           IMPORTANT: READ BEFORE DOWNLOADING, COPYING AND USING.             #
+#                                                                              #
+#                                                                              #
+#      Copyright [2017] [ShenZhen Longer Vision Technology], Licensed under    #
+#      ******** GNU General Public License, version 3.0 (GPL-3.0) ********     #
+#      You are allowed to use this file, modify it, redistribute it, etc.      #
+#      You are NOT allowed to use this file WITHOUT keeping the License.       #
+#                                                                              #
+#      Longer Vision Technology is a startup located in Chinese Silicon Valley #
+#      NanShan, ShenZhen, China, (http://www.longervision.cn), which provides  #
+#      the total solution to the area of Machine Vision & Computer Vision.     #
+#      The founder Mr. Pei JIA has been advocating Open Source Software (OSS)  #
+#      for over 12 years ever since he started his PhD's research in England.  #
+#                                                                              #
+#      Longer Vision Blog is Longer Vision Technology's blog hosted on github  #
+#      (http://longervision.github.io). Besides the published articles, a lot  #
+#      more source code can be found at the organization's source code pool:   #
+#      (https://github.com/LongerVision/OpenCV_Examples).                      #
+#                                                                              #
+#      For those who are interested in our blogs and source code, please do    #
+#      NOT hesitate to comment on our blogs. Whenever you find any issue,      #
+#      please do NOT hesitate to fire an issue on github. We'll try to reply   #
+#      promptly.                                                               #
+#                                                                              #
+#                                                                              #
+# Version:          0.0.1                                                      #
+# Author:           JIA Pei                                                    #
+# Contact:          jiapei@longervision.com                                    #
+# URL:              http://www.longervision.cn                                 #
+# Create Date:      2017-03-20                                                 #
+################################################################################
+
 import numpy as np
 import cv2
 import yaml
@@ -21,7 +56,7 @@ while(found < 10):  # Here, 10 can be changed to whatever number you like to cho
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -43,6 +78,8 @@ cap.release()
 cv2.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+
 # It's very important to transform the matrix to list.
 data = {'camera_matrix': np.asarray(mtx).tolist(), 'dist_coeff': np.asarray(dist).tolist()}
 
